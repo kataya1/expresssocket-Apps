7d37f71f78ebb073030c8cc74a928291eb722851
kataya <kataya@katayas-MacBook-Pro.local>
2023-09-19 11:37:20 -0400

    second demo project complete

--- demoAppNservers.js
+++ demoAppNservers.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const app = express();
-const expressWs = require("expresssocket")(app);
-const websocket = require('ws')
+const expressWs = require("express-socket-routes")(app);
+const WebSocket = require('ws');
 
-let server = websocket.Server()
-app.sRoute("/room/:id", (req, socket, head) => { })
+const webSocketServers = {};
 
-app.listen(3000, () => {
-    console.log('app is listening on 3000')
-})
+
+app.sRoute('/room/:id', (req, socket, head) => {
+
+    const roomId = req.params.id;
+
+    if (!webSocketServers[roomId]) {
+        webSocketServers[roomId] = new WebSocket.Server({ noServer: true });
+
+        webSocketServers[roomId].on('connection', (ws) => {
+
+            ws.on('message', (message) => {
+                webSocketServers[roomId].clients.forEach(client => {
+                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                        client.send(message.toString());
+                    }
+                });
+            });
+
+            ws.on('close', () => {
+
+            });
+        });
+    }
+
+    webSocketServers[roomId].handleUpgrade(req, socket, head, (ws) => {
+
+        webSocketServers[roomId].emit('connection', ws);
+    });
+
+});
+
+const server = app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
